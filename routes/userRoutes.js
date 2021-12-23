b4f3acd03a57d9d807f543188f2a6de1efb217ab
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2021-12-23 18:20:15 -0300

    sda

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const {getAllUsers,deleteUser,updateUser,register,login}  = require("../controllers/usercontrollers")
+const {getAllUsers,updateUser,register,login}  = require("../controllers/usercontrollers")
 const UserRouter = express.Router()
 
 UserRouter.get("/all",getAllUsers)
@@ -9,7 +9,5 @@ UserRouter.post("/register",register)
 UserRouter.post("/login",login)
 
 UserRouter.put("/",updateUser)
-
-UserRouter.delete("/",deleteUser)
 
 module.exports = UserRouter
